b2ff4136e1a05cf4372b5262b89612655bd2f127
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2016-04-15 09:20:37 +0300

    Small modifications

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name = 'hokuyolx',
